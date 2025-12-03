2f9630150fc1c1a57ff30e576e27c17783843803
mist-miguel-lara <123275349+mist-miguel-lara@users.noreply.github.com>
2025-12-02 17:04:26 -0800

    final exam practice

---
+++ index.mjs
@@ -0,0 +1,37 @@
+import 'dotenv/config'; //importing .env environment values
+
+import express from 'express';
+import mysql from 'mysql2/promise';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//for Express to get values using the POST method
+app.use(express.urlencoded({extended:true}));
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', (req, res) => {
+   console.log("Password " + process.env.DB_PASSWORD);
+   console.log("User " + process.env.DB_USER);
+
+   res.send('Hello Express app!')
+});
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
